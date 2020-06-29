b365d8d4fe80a5ddd006656204bde4b52a74d6c0
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-06-28 17:26:33 -0700

    "finishes jump_game_iii.py and adds runtime analysis graph"

--- jump_game_iii.py
+++ jump_game_iii.py
@@ -1,139 +1,131 @@
-# from random import randrange, seed
-# import numpy as np
-#
-#
-# class Solution(object):
-#     def __init__(self):
-#         """
-#         """
-#         pass
-#
-#     def can_reach(self, arr, start):
-#         """
-#         :type arr: List[int]
-#         :type start: int
-#         :rtype: bool
-#         returns whether a zero can be reached by moving indecies
-#         forwards or backwards determined by the number (int) at the current index
-#         """
-#         answer = self.brute(arr, start)
-#
-#     def brute(self, arr, start):
-#         """
-#         :type arr: List[int]
-#         :type start: int
-#         :rtype: bool
-#         does a brute force can_reach() search
-#         binary search, where we branch and try both above and below
-#         """
-#         x = arr[start]
-#         if x == 0:
-#             return True
-#         elif start - x >= 0:
-#             left_answer = self.brute(arr, start - x)
-#             if left_answer == True:
-#                 return True
-#         elif start + x < len(arr):
-#             right_answer = self.brute(arr, start + x)
-#             if right_answer == True:
-#                 return True
-#         else:
-#             return False
-#
-#     def test(self, length=10):
-#         """
-#         :type length: int
-#         :rtype: None
-#         generates and prints out random seed arrays used, start_index,
-#         tests can_reach() for each array and prints
-#         """
-#         assert 1 <= length and length <= 5 * 10 ^ 4, "\
-#         length of list is out of the range of 1 <= arr.length <= 5 * 10^4"
-#         for sd in range(10):
-#             arr = self.make_list(length, sd)
-#             start = self.start_index(length, sd)
-#             print(arr, start)
-#             cr = self.can_reach(arr, start)
-#             print(cr)
-#
-#     def make_list(self, length=10, sd=1):
-#         """
-#         :type length: int
-#         :type sd: int
-#         :rtype: List[int]
-#         creates a list which follows the following constraint
-#         0 <= arr[i] < arr.length
-#         """
-#         seed(sd)
-#         arr = [randrange(0, length) for x in range(length)]
-#         arr = np.array(arr)
-#         return arr
-#
-#     def start_index(self, length=10, sd=1):
-#         """
-#         :type length: int
-#         :type sd: int
-#         :rtype: int
-#         creates a start index which follows the following constraint
-#         0 <= start < arr.length
-#         """
-#         seed(sd)
-#         start = randrange(0, length)
-#         return start
 import numpy as np
 import random
-def make_list(length=10):
-	"""
-	:type length: int
-	:rtype: numpy.array(List[int])
-	:dependencies: numpy, random
-	creates a np.array with length random integers between 0,length (inclusive)
-	"""
-	arr = np.array([random.randint(0,length) for i in range(length)])
-	return arr
-def rand_i(length=10):
-	"""
-	:type length: int
-	:rtype: List[int]
-	return an starting int index between [0,length) in list form
-	"""
-	i = random.randint(0, length-1)
-	return [i]
-def binary_search(arr, index_list):
-	"""
-	:type arr: numpy.array(List[int])
-	:type index_list: List[int]
-	:rtype: bool
-	returns true when arr[i]==0
-	returns false when repeat index, or out of bounds moving both left and right.
-	"""
-	# print("ir",index_list)
-	i = index_list[-1]
-	# print("i", i)
-	x =  arr[i]
-	if x == 0:
-		return True
-	left = i - x
-	right = i + x
-	if left>=0 and left<len(arr) and left not in index_list:
-		left_index_list = index_list.append(left)
-		# print("left",left_index_list)
-		bs = binary_search(arr,left_index_list)
-		if bs:
-			return True
-	if right>=0 and right<len(arr) and right not in index_list:
-		right_index_list=index_list.append(right)
-		bs = binary_search(arr,right_index_list)
-		if bs:
-			return True
-	return False
-def test():
-	length = 10
-	for x in range(100):
-		arr = make_list(length)
-		print(arr)
-		index_list = rand_i(length)
-		bs = binary_search(arr, index_list)
-		print(bs)
+from time import time
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+# from scipy.optimize import curve_fit
+class Solution(object):
+	def __init__(self):
+		self.test_string = """
+		average runtime, {0: .2E} 
+		for binary_search with lists of length 10**{1:.2E}
+		and number of runs at {2}, 
+		likewise the standard deviation is {3:.2E},
+		and the total run time including testing setup for this length is {4:.2E}
+		"""
+		self.test()
+	def make_list(self, length=10):
+		"""
+		:type length: int
+		:rtype: numpy.array(List[int])
+		:dependencies: numpy, random
+		creates a np.array with length random integers between 0,length (inclusive)
+		uses random.random and type casts it to achieve a ~10X speed boost 
+		over random.randint(0,length) when creating these lists
+		random.sample(range(length),length) forgoes replacement, 
+		and so another option is needed
+		uses np.randint(args) over [int(length * random.random()) for i in range(length)]
+		for a increase from 1.53msec to 170 usec with numpy 
+		(the numpy will also have better memory usage, if I remember correctl)
+		this switches runtime for the length of 10**5 from 161sec to 1sec
+		"""
 
-test()
+		arr = np.random.randint(0,length,length,int)
+
+		return arr
+	def rand_i(self, length):
+		"""
+		:type length: int
+		:rtype: List[int]
+		return an starting int index between [0,length) in list form
+		"""
+		i = random.randrange(0, length)
+		return [i]
+	def binary_search(self, arr, index_list):
+		"""
+		:type arr: numpy.array(List[int])
+		:type index_list: List[int]
+		:rtype: bool
+		returns true when arr[i]==0
+		returns false when repeat index, or out of bounds moving both left and right.
+		recursion max can occur (on macbook_pro 2015: 1000) at length=10**7
+		"""
+		i = index_list[-1]
+		x =  arr[i]
+		if x == 0:
+			# print("correct path:", index_list)
+			return True
+		left = i - x
+		right = i + x
+		#recursions with left and right step checking false situations in if statement
+		if left>=0 and left<len(arr) and left not in index_list:
+			index_list.append(left)
+			# print("left",left_index_list)
+			bs = self.binary_search(arr,index_list)
+			if bs:
+				return True
+		if right>=0 and right<len(arr) and right not in index_list:
+			index_list.append(right)
+			bs = self.binary_search(arr,index_list)
+			if bs:
+				return True
+		return False
+	def std_dev(self, time_avg, time_list):
+		variance = 0
+		for te in time_list:
+			variance += (time_avg-te)**2
+		std_dev = np.sqrt(variance)/len(time_list)
+		return std_dev
+	def plot_times(self,time_stats):
+		#linear regression fitting and data reformatting
+		print("running LinearRegression")
+		time_stats = np.array(time_stats)
+		lr = LinearRegression()
+		lengths = time_stats[:,0]
+		X = np.reshape(lengths,(-1,1))
+		time_averages = time_stats[:,1]
+		time_std_dev= time_stats[:,2]
+		lr.fit(X, time_averages)
+		r_squared = lr.score(X, time_averages)
+		lr_label = "linear R**2= {0:.5f}".format(r_squared)
+		#plotting
+		print("plotting")
+		plt.scatter(lengths, time_averages, color="red", label="average times raw data")
+		plt.errorbar(lengths, time_averages, yerr=time_std_dev, linewidth=0,
+			elinewidth=2, color="red", label="standard deviations for time data raw")
+		plt.plot(lengths, lr.predict(X), color="green", label=lr_label)
+
+		# plt.plot(lengths, logr.predict(X), color="blue")
+		plt.title("binary_search as a function of length")
+		plt.xlabel("Length")
+		plt.ylabel("Average Time of binary_search")
+		plt.legend()
+		plt.show()
+	def test(self):
+		time_stats = []
+		print("executing code")
+		for xp in np.arange(1,6,0.1): 
+			#setting up constants for test and run parameters
+			length = int(10**xp)
+			time_list=[]
+			runs = 5*10**3
+			tt = time()
+			for x in range(runs):
+				#running code and finding elapsed time for binary_search
+				arr = self.make_list(length)
+				index_list = self.rand_i(length)
+				t = time()
+				bs = self.binary_search(arr, index_list)
+				te = time() - t
+				time_list.append(te)
+			#time stats calculations and printing stats
+			time_total = sum(time_list)
+			time_avg = time_total/len(time_list)
+			time_std_dev = self.std_dev(time_avg, time_list)
+			time_stats.append([length, time_avg, time_std_dev])
+			test_setup_time = time()-tt - time_total
+			# print(self.test_string.format(time_avg, length, runs, time_std_dev, test_setup_time))
+		#plotting the timestats
+		self.plot_times(time_stats)
+Solution()
+
